c1c7f6891ad78fb3c355c2815e181b85a8551393
boussaamehdi09 <boussaa.mehdi@franchettisalviani.net>
2025-11-04 22:30:40 +0100

    jfjh

--- Esercizi_Array/Program.cs
+++ Esercizi_Array/Program.cs
@@ -7,18 +7,29 @@
             // es n:1
             int[] num = { 1, 54, 7, 4, 3 };
             for (int i = num.Length - 1; i >= 0; i--) 
-            { 
-                Console.WriteLine(num[i]);
+            {
+                Console.WriteLine("[" + num[i] + "]");
             }
             Console.WriteLine("-------------------------------------------");
             // es n:2
-            int[] num2 = { 1, 113, 4, 6, 7, 8, 100 };
-            for(int a = 2; a <= 0; a++) 
+            int[] nP = { 1, 54, 7, 4, 3 };
+            for (int i = 0; i < nP.length; i++)
             {
-                Console.WriteLine(num2[a]);
-            
-            
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine("[" + nP[i] + "]");
+                }
             }
+            Console.WriteLine("-------------------------------------------");
+            int[] nD = { 1, 54, 7, 4, 3 };
+            for (int i = 0; i < nD.length; i++)
+            {
+                if (nD[i] % 2 == 1)
+                {
+                    Console.WriteLine("[" + nD[i] + "]");
+                }
+            }
+
         }
     }
 }
