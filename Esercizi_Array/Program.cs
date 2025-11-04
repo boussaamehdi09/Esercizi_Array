21f6bf10615cffe0657d3668006dd3e7ce4b3c28
boussaamehdi09 <boussaa.mehdi@franchettisalviani.net>
2025-11-04 10:55:54 +0100

    Aggiungere i file di progetto.

---
+++ Esercizi_Array/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Esercizi_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // es n:1
+            int[] num = { 1, 54, 7, 4, 3 };
+            for (int i = num.Length - 1; i >= 0; i--) 
+            { 
+                Console.WriteLine(num[i]);
+            }
+            Console.WriteLine("-------------------------------------------");
+            // es n:2
+            int[] num2 = { 1, 113, 4, 6, 7, 8, 100 };
+            for(int a = 2; a <= 0; a++) 
+            {
+                Console.WriteLine(num2[a]);
+            
+            
+            }
+        }
+    }
+}
