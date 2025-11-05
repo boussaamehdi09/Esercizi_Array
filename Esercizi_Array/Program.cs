e2c30f05606bfd47a0553e59b538ef9b071cd7d8
boussaamehdi09 <boussaa.mehdi@franchettisalviani.net>
2025-11-05 22:26:12 +0100

    hgfjh

--- Esercizi_Array/Program.cs
+++ Esercizi_Array/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Esercizi_Array
+﻿using Microsoft.Win32.SafeHandles;
+
+namespace Esercizi_Array
 {
     internal class Program
     {
@@ -29,6 +31,40 @@
                 {
                     Console.WriteLine("[" + nD[i] + "]");
                 }
+            }
+            // es n:6
+            Console.WriteLine("-------------------------------------------");
+            int[] v1 = { 1, 54, 7, 4, 3 };
+            int x = 5, cMag = 0, cMin = 0;
+            for(int i=0;i < v1.Length; i++)
+            {
+                if (v1[i] < x)
+                {
+                    cMin = cMin + 1;
+                }
+                cMag = cMag + 1;
+            }
+            int[] v2 = new int[cMin];
+            int[] v3 = new int[cMag];
+            int iMag = 0, iMin = 0;
+            for (int i = 0; i < v1.Length; i++)
+            {
+                if (v1[i] < x)
+                {
+                    v2[iMin] = v1[i];
+                    iMin = iMin + 1;
+                }
+                v3[iMag] = v1[i];
+                iMag = iMag + 1;
+            }
+            for (int i = 0; i < v2.Length; i++)
+            {
+                Console.WriteLine($"[{v2[i]}");
+            }
+
+            for (int i = 0; i < v3.Length; i++)
+            {
+                Console.WriteLine($"[{v3[i]}");
             }
         }
     }
