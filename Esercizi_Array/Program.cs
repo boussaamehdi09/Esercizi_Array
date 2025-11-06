d8ecdc2d904b44f5214ef7f74a87b91f49cb5eb7
boussaamehdi09 <boussaa.mehdi@franchettisalviani.net>
2025-11-06 10:51:39 +0100

    Update Program.cs
    hj.gf

--- Esercizi_Array/Program.cs
+++ Esercizi_Array/Program.cs
@@ -34,7 +34,7 @@ namespace Esercizi_Array
                 }
             }
             // es n:6
-            Console.WriteLine("----------------------es n:6----------------------");
+            Console.WriteLine("----------------------es n:4----------------------");
             int[] v1 = { 1, 54, 7, 4, 3 };
             int x = 5, cMag = 0, cMin = 0;
             for (int i = 0; i < v1.Length; i++)
@@ -73,6 +73,35 @@ namespace Esercizi_Array
             {
                 Console.WriteLine($"[{v3[i]}]");
             }
+            // es n:medio
+            Console.WriteLine("----------------------es n:medio----------------------");
+            int[] vet1 = { 1, 2, 3, 4, 5 };
+            int[] vet2 = { 6, 7, 8, 9, 10 };
+            int[] vU = new int [vet1.Length + vet2.Length];
+
+            for (int i = 0; i < vet1.Length; i++)
+            {
+               .
+            }
+            for (int i = 0; i < vet2.Length; i++)
+            {
+                .
+            }
+            for (int i = 0; i < vU.Length; i++)
+            {
+                .
+            }
+            // es n:facile
+            Console.WriteLine("----------------------es n:facile----------------------");
+            int[] vitt1 = { 1, 54, 7, 4, 3 };
+            int sV = 0;
+            int min = vitt1[0];
+            int max = vitt1[0];
+            int iMax = 0;
+            int iMIn = 0;
+
+
+
         }
     }
 }
