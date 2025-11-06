181563e87bf6d4111f85377159664d44de02ff30
boussaamehdi09 <boussaa.mehdi@franchettisalviani.net>
2025-11-06 10:09:02 +0100

    Update Program.cs
    asss

--- Esercizi_Array/Program.cs
+++ Esercizi_Array/Program.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Win32.SafeHandles;
+using Microsoft.Win32.SafeHandles;
 
 namespace Esercizi_Array
 {
@@ -7,12 +7,13 @@ namespace Esercizi_Array
         static void Main(string[] args)
         {
             // es n:1
+            Console.WriteLine("----------------------es n:1----------------------");
             int[] num = { 1, 54, 7, 4, 3 };
             for (int i = num.Length - 1; i >= 0; i--)
             {
                 Console.WriteLine("[" + num[i] + "]");
             }
-            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("----------------------es n:2----------------------");
             // es n:2
             int[] nP = { 1, 54, 7, 4, 3 };
             for (int i = 0; i < nP.Length; i++)
@@ -23,7 +24,7 @@ namespace Esercizi_Array
                 }
             }
             // es n:3
-            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("----------------------es n:3----------------------");
             int[] nD = { 1, 54, 7, 4, 3 };
             for (int i = 0; i < nD.Length; i++)
             {
@@ -33,16 +34,19 @@ namespace Esercizi_Array
                 }
             }
             // es n:6
-            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("----------------------es n:6----------------------");
             int[] v1 = { 1, 54, 7, 4, 3 };
             int x = 5, cMag = 0, cMin = 0;
-            for(int i=0;i < v1.Length; i++)
+            for (int i = 0; i < v1.Length; i++)
             {
-                if (v1[i] < x)
+                if (v1[i] <= x)
                 {
                     cMin = cMin + 1;
                 }
-                cMag = cMag + 1;
+                else
+                {
+                    cMag = cMag + 1;
+                }
             }
             int[] v2 = new int[cMin];
             int[] v3 = new int[cMag];
@@ -54,17 +58,20 @@ namespace Esercizi_Array
                     v2[iMin] = v1[i];
                     iMin = iMin + 1;
                 }
-                v3[iMag] = v1[i];
-                iMag = iMag + 1;
+                else
+                {
+                    v3[iMag] = v1[i];
+                    iMag = iMag + 1;
+                }
             }
             for (int i = 0; i < v2.Length; i++)
             {
-                Console.WriteLine($"[{v2[i]}");
+                Console.WriteLine($"[{v2[i]}]");
             }
-
+            Console.WriteLine("----");
             for (int i = 0; i < v3.Length; i++)
             {
-                Console.WriteLine($"[{v3[i]}");
+                Console.WriteLine($"[{v3[i]}]");
             }
         }
     }
